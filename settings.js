e222731b3fdb000485af5843f9822be9285b824a
XiaTech <nishanthadamith03@gmail.com>
2026-07-27 23:19:25 +0530

    Update SESSION_ID default value in settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Y𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=2ed89bf2d8baddb7' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
